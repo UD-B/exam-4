be1e9262b9f320e75b41e567da7a17ee4c9ddf25
UD <yudibarak770@gmail.com>
2025-11-13 12:36:20 +0200

    functions

--- functions.py
+++ functions.py
@@ -36,17 +36,34 @@ def defence(text: str):
     half = 0
     is_even = None
     if len(text) % 2 != 0:
-        half = (len(text) + 1) / 2
+        half = int((len(text) + 1) / 2)
+        is_even = False
     else:
-        half = len(text) / 2
+        half = int(len(text) / 2)
+        is_even = True
     first_half = text[:half]
     second_half = text[half:]
     for i in first_half:
         even.append(i)
     for i in second_half:
         odd.append(i)
-    for i in range(len(text)):
-        if i % 2 == 0:
-            new_text += even.pop()
-        else:
-            new_text += odd.pop()
+    if is_even:
+        for i in range(len(text)):
+            if i % 2 == 0:
+                new_text += even.pop(0)
+            else:
+                new_text += odd.pop(0)
+    else:
+        for i in range(len(text)):
+            if i % 2 == 0:
+                new_text += even.pop(0)
+            else:
+                new_text += odd.pop(0)
+    return new_text
+    
+    
+
+print(fence("borak"))
+print(defence("brkoa"))
+print(fence("yehuda"))
+print(defence("yhdeua"))
